51d6ee8d64ce1fb0d4ed260a48255d42d30241cc
正权 徐 <xuzhengquan@yundianjia.com>
2017-12-22 18:47:37 +0800

    setting

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'assets/main.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        chunkFilename: "[id].chunk.js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: 2
+        })
+    ],
+
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        port: 9001,
+        hot: true,
+        
+    }
+}
