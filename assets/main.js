d7132d8c5fd0a84cec3e40dfabc703fe92696feb
正权 徐 <xuzhengquan@yundianjia.com>
2017-12-25 19:09:27 +0800

    add necessary loader and plugin

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,10 @@
-console.log('hehehehe!!!!!')
+const styles =  require('./main.css')
+
+var img = document.createElement("img");
+img.src = require("./browserify.png");
+document.body.appendChild(img);
+
+var h1 = document.createElement("h1");
+h1.className = styles.h1;
+h1.innerText = 'browserify'
+document.body.appendChild(h1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,88 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: path.resolve(__dirname, 'assets/main.js'),
+    entry:
+        [
+            path.resolve(__dirname, 'assets/main.js'),
+        ],
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/',
-        filename: '[name].bundle.js',
+        filename: '[name].[chunkhash].js',
         chunkFilename: "[id].chunk.js"
     },
 
     module: {
         rules: [
+            // js es6 => es5
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             },
+
+            // css-loader 解析css, style-loader 将样式插入到style标签 modules：css-modules 模块化css文件，防止污染全局
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader?modules', 'css-loader?modules']
+            },
+
+            // 样式预编译
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+
+            // require img in js file, return DateURL
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: 'img/[name].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     },
     
     plugins: [
+        // 公用模块打包成chunk
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
             minChunks: 2
+        }),
+        // css单独打包成chunk
+        new ExtractTextPlugin({
+            filename: "[name].bundle.css",
+            allChunks: true
+        }),
+
+        // minify output js codes
+        new UglifyJsPlugin({
+            cache: true
+        }),
+
+        // generate html5 file for you that includes all your webpack bundles in the body using script tags.
+        new HtmlWebpackPlugin({
+            template: './assets/index.html'
         })
     ],
 
     devServer: {
         contentBase: path.resolve(__dirname, './dist'),
-        port: 9001,
-        hot: true,
-
+        publicPath: '/',
+        port: 9001
     }
 }
